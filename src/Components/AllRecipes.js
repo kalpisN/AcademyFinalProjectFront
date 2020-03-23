b670a3cffe3373dab0f876035946df51e45df335
PihlaParkkinen <pihla.parkkinen@gmail.com>
2020-03-23 14:59:50 +0200

    nav bar and live search started

--- src/Components/AllRecipes.js
+++ src/Components/AllRecipes.js
@@ -5,6 +5,7 @@ class AllRecipes extends Component {
     render() {
         return (
             <div>
+
                 <h1>Tässä on etusivukomponentti, joka näyttää kaikki reseptit</h1>
                 <Search />
                 <p>Tänne ne reseptit tulee</p>
