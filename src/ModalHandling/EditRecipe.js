12033a40ec547dc29b8e6b9a374e6cfc4abb6451
NooraKalpio <noora.kalpio@gmail.com>
2020-03-27 23:38:16 +0200

    recipe delete, en tiedä toimiiko vielä

--- src/ModalHandling/EditRecipe.js
+++ src/ModalHandling/EditRecipe.js
@@ -3,10 +3,15 @@ import Form from 'react-bootstrap/Form'
 import Button from "react-bootstrap/Button";
 import './EditRecipe.css';
 import Modal from "react-bootstrap/Modal";
+import DeleteRecipe from "../Recipes/DeleteRecipe";
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import Card from "react-bootstrap/Card";
 
 
 
 function EditRecipe(props) {
+
+    const [deleteModalShow, setDeleteModalShow] = React.useState(false);
 
         return (
             <Modal
@@ -29,9 +34,16 @@ function EditRecipe(props) {
                 </Modal.Body>
                 <Modal.Footer>
                     <Button onClick={props.onHide}>Close</Button>
+                    <Button variant="light" onClick={() => setDeleteModalShow(true)}><DeleteForeverIcon/></Button>
                 </Modal.Footer>
                 </Form>
+                <DeleteRecipe
+                    name={props.name}
+                    show={deleteModalShow}
+                    onHide={() => setDeleteModalShow(false)}
+                />
             </Modal>
+
         );
 }
 export default EditRecipe;
--- src/Recipes/DeleteRecipe.js
+++ src/Recipes/DeleteRecipe.js
@@ -1,13 +1,52 @@
 import React, {Component} from "react";
 import {API_BASE_URL} from "../Helpers/API";
+import Message from "../Helpers/Message";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 
-async function DeleteRecipe(props) {
+function DeleteRecipe(props) {
 
-    const url = API_BASE_URL + '/recipes/' + props.id;
-        await fetch(url, {
-            method: 'DELETE'
-        }).then(r => r.json())
+        const [RecipeDelete, setRecipeDelete] = React.useState(false);
 
+        if (RecipeDelete === true) {
+
+            const url = API_BASE_URL + '/recipes/' + props.name;
+
+            fetch(url, {
+                method: 'DELETE'
+            }).then(r => r.json())
+
+            alert(props.id + 'poistettu')
+
+        }
+
+
+
+    return(
+
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Haluatko todella poistaa reseptin?
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Button variant="light" onClick={() => setRecipeDelete(true)}>Kyllä</Button>
+                    <Button variant="light" onClick={props.onHide}>Ei</Button>
+                </Modal.Body>
+                <Modal.Footer>
+                </Modal.Footer>
+        </Modal>
+
+
+    )
 }
 
 export default DeleteRecipe;
