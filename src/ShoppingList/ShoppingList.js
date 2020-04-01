58aa55ed2d2d31cd7c769ee093c0622b65b7bd87
JennaKinos <jenna.kinos@gmail.com>
2020-04-01 12:13:42 +0300

    putsatkaa Shoppinglist tiedoston sisältö ennen ku pullaatte tän

--- src/ShoppingList/ShoppingList.js
+++ src/ShoppingList/ShoppingList.js
@@ -1,103 +1,41 @@
-import React, {Component} from 'react';
-import update from "immutability-helper";
-import {Row, Table} from "react-bootstrap";
-import {Checkbox} from "@material-ui/core";
-import MyCheckbox from "./MyCheckbox";
-
-let myOptions=["1","2","3"];
-
-class ShoppingList extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state={
-            checkboxes: myOptions.reduce(
-                (options,option) => ({
-    ...options,
-                    [option]: false}), {})
-        };
-    }
+import React, {useEffect, useState} from 'react';
+import {API_BASE_URL} from "../Helpers/API";
+import axios from "axios";
+import "./ShoppingList.css"
 
 
-    selectAllCheckboxes = isSelected => {
-        Object.keys(this.state.checkboxes).forEach(checkbox => {
-            // BONUS: Can you explain why we pass updater function to setState instead of an object?
-            this.setState(prevState => ({
-                checkboxes: {
-                    ...prevState.checkboxes,
-                    [checkbox]: isSelected
-                }
-            }));
-        });
-    };
+const ShoppingList = ()=> {
 
-    selectAll = () => this.selectAllCheckboxes(true);
 
-    deselectAll = () => this.selectAllCheckboxes(false);
+    const url = API_BASE_URL + '/shoppingList';
+    const [shoppingList, setShoppingList] = useState([])
 
-    handleCheckboxChange = changeEvent => {
-        const { name } = changeEvent.target;
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get(url)
+            .then(response => {
+                console.log('promise fulfilled')
+                setShoppingList(response.data)
+            })
+    }, [])
+    console.log('render', shoppingList.length, 'items')
 
-        this.setState(prevState => ({
-            checkboxes: {
-                ...prevState.checkboxes,
-                [name]: !prevState.checkboxes[name]
-            }
-        }));
-    };
+    const shoppingListRows =
+        shoppingList.map(item =>
 
-    handleFormSubmit = formSubmitEvent => {
-        formSubmitEvent.preventDefault();
+            <tr key={item.name}>
+                <td><input type="checkbox" className='checkBox' id={item.name} name='item' value={item}/></td>
+                <td><label form='shoppingList' className='checkBoxLabel'>{item.name}</label></td>
+                <td>{item.amount}</td>
+                <td>{item.unit}</td>
+            </tr>
+        )
 
-        Object.keys(this.state.checkboxes)
-            .filter(checkbox => this.state.checkboxes[checkbox])
-            .forEach(checkbox => {
-                console.log(checkbox, "is selected.");
-            });
-    };
 
-    createCheckbox = option => (
-        <MyCheckbox
-            label={option}
-            isSelected={this.state.checkboxes[option]}
-            onCheckboxChange={this.handleCheckboxChange}
-            key={option}
-        />
-    );
-
-    createCheckboxes = () => myOptions.map(this.createCheckbox);
-
-    render() {
-        return (
-            <div className="container">
-                <div className="row mt-5">
-                    <div className="col-sm-12">
-                        <form onSubmit={this.handleFormSubmit}>
-                            {this.createCheckboxes()}
-
-                            <div className="form-group mt-2">
-                                <button
-                                    type="button"
-                                    className="btn btn-outline-primary mr-2"
-                                    onClick={this.selectAll}>
-                                    Select All
-                                </button>
-                                <button
-                                    type="button"
-                                    className="btn btn-outline-primary mr-2"
-                                    onClick={this.deselectAll}
-                                >
-                                    Deselect All
-                                </button>
-                                <button type="submit" className="btn btn-primary">
-                                    Save
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+    return (
+        <div>{shoppingListRows}</div>
+    )
 }
-export default ShoppingList;
+
+export default ShoppingList
